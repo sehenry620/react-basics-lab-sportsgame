0f9c2f4ff62161cba5a3883f4c48823972abab04
Henry <henrstep@amazon.com>
2019-11-05 15:45:55 -0500

    update files

--- reactApp.js
+++ reactApp.js
@@ -1,10 +1,47 @@
+class Team extends React.Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      shots: 0,
+      score: 0
+    }
+  }
+
+  shotHandler = () => {
+    if(Math.random() > .5){
+      this.setState((state, props) => ({
+        score: state.score + 1
+      }));
+    }
+    this.setState((state, props) => ({
+      shots: state.shots + 1 
+      
+      
+    }));
+  }
+
+
+
+  render() {
+    return (<h1>
+      {this.props.name}
+      <img alt="team logo" width="250" src={this.props.logo} />
+      shots: {this.state.shots}
+      score: {this.state.score}
+
+      <button onClick= {this.shotHandler}>Shoot</button>
+    </h1>)
+  }
+
+}
 // Deafault App component that all other compents are rendered through
-function App(props){
+function App(props) {
   return (
     <div>
-      <h1>Welcome to the sports game starter</h1>
-      This file represents the code after completing the setup step in the lab instructions
+      <Team name="Gainesville Giraffes" logo="giraffelogo.jpg" />
+      <Team name="Zionsville Zebras" logo="zebralogo.jpg" />
     </div>
   )
 }
